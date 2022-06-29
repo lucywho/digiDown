245367b1101d66f58b8a0464a66c98d8675ad291
Lucy Toman <lucy@toman.me.uk>
2022-06-29 13:01:11 +0200

    adds custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+        <>
+            <h1 className="h-60 flex flex-col justify-between items-center text-xl">
+                <p>+++OUT OF CHEESE ERROR+++</p>
+                <p>404 PAGE NOT FOUND</p>
+                <p>+++REDO FROM START+++</p>
+            </h1>
+        </>
+    )
+}
